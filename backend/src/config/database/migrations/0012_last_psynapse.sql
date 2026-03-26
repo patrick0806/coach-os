f53e12cd391064e6ab60dadecbed3ff8dcba43a9
Patrick Nicezi <patrickk0806@gmail.com>
2026-03-26 11:02:12 -0300

    feat: log erros on migration step

--- backend/src/config/database/migrations/0012_last_psynapse.sql
+++ backend/src/config/database/migrations/0012_last_psynapse.sql
@@ -1,6 +1,6 @@
-DROP TABLE "appointment_requests" CASCADE;--> statement-breakpoint
-DROP TABLE "appointments" CASCADE;--> statement-breakpoint
-DROP TABLE "availability_exceptions" CASCADE;--> statement-breakpoint
-DROP TABLE "availability_rules" CASCADE;--> statement-breakpoint
-DROP TABLE "training_schedule_exceptions" CASCADE;--> statement-breakpoint
-DROP TABLE "training_schedules" CASCADE;
+DROP TABLE IF EXISTS "appointment_requests" CASCADE;--> statement-breakpoint
+DROP TABLE IF EXISTS "appointments" CASCADE;--> statement-breakpoint
+DROP TABLE IF EXISTS "availability_exceptions" CASCADE;--> statement-breakpoint
+DROP TABLE IF EXISTS "availability_rules" CASCADE;--> statement-breakpoint
+DROP TABLE IF EXISTS "training_schedule_exceptions" CASCADE;--> statement-breakpoint
+DROP TABLE IF EXISTS "training_schedules" CASCADE;
